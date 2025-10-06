cd156cc48f518c851d21829a52576d68b83189b5
henrrepereira <henrre37@gmail.com>
2025-10-05 22:25:14 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
-// Configuração do Firebase exportada como variável global
-window.firebaseConfig = {
-  apiKey: "AIzaSyBnit6_u1ZZG4Mg1Ma4q0OeTqeos5mJQ",
-  authDomain: "diario-de-bordo-tecnico.firebaseapp.com",
-  projectId: "diario-de-bordo-tecnico",
-  storageBucket: "diario-de-bordo-tecnico.appspot.com",
-  messagingSenderId: "398639660413",
-  appId: "1:398639660413:web:ac182a4ea9e0f90edb2014",
-  measurementId: "G-5HT50J72E2"
-};
+  // Configuração do Firebase exportada como variável global
+  window.firebaseConfig = {
+    apiKey: "AIzaSyBnit6_u1ZZG4Mg1Ma4q9c0OeTqeos5mJQ",
+    authDomain: "diario-de-bordo-tecnico.firebaseapp.com",
+    projectId: "diario-de-bordo-tecnico",
+    storageBucket: "diario-de-bordo-tecnico.firebasestorage.app",
+    messagingSenderId: "398639660413",
+    appId: "1:398639660413:web:ac182a4ea9e0f90edb2014",
+    measurementId: "G-5HT50J72E2"
+  };
